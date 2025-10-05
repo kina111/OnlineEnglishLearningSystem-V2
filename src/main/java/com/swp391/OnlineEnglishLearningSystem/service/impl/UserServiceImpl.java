3e5e242a80d5f286aa8de702e669c212aa2f5703
Nguyen Kim Nam <namthptcvp@gmail.com>
2025-10-05 10:21:37 +0700

    #9 - Logic for update Password

--- src/main/java/com/swp391/OnlineEnglishLearningSystem/service/impl/UserServiceImpl.java
+++ src/main/java/com/swp391/OnlineEnglishLearningSystem/service/impl/UserServiceImpl.java
@@ -48,12 +48,18 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public Optional<User> findByEmailAndEnabledTrue(String email) {
-        return this.userRepository.findByEmailAndEnabledTrue(email);
+    public User findByEmailAndEnabledTrue(String email) {
+        Optional<User> user = userRepository.findByEmailAndEnabledTrue(email);
+        return user.orElse(null);
     }
 
     @Override
     public void updatePassword(User user, String password) {
         user.setPassword(passwordEncoder.encode(password));
+    }
+
+    @Override
+    public boolean isOldPasswordCorrect(User currentUser, String oldPassword) {
+        return passwordEncoder.matches(oldPassword, currentUser.getPassword());
     }
 }
