3cfc65464567f5398310292f04a466f7a97af737
Nguyen Kim Nam <namthptcvp@gmail.com>
2025-10-06 08:08:24 +0700

    #13 - config resource handler and properties for uploading files

--- src/main/java/com/swp391/OnlineEnglishLearningSystem/config/WebConfig.java
+++ src/main/java/com/swp391/OnlineEnglishLearningSystem/config/WebConfig.java
@@ -1,4 +1,16 @@
 package com.swp391.OnlineEnglishLearningSystem.config;
 
-public class WebConfig {
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Expose folder uploads
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/"); // đường dẫn tới folder uploads
+    }
 }
