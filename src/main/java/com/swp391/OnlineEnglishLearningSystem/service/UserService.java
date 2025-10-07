006cb601da613ca729e23f08e4203fb010238c9a
Nguyen Kim Nam <namthptcvp@gmail.com>
2025-10-07 09:10:38 +0700

    #15 - Update UserService to process, paginate, sort, filter, and search options

--- src/main/java/com/swp391/OnlineEnglishLearningSystem/service/UserService.java
+++ src/main/java/com/swp391/OnlineEnglishLearningSystem/service/UserService.java
@@ -3,7 +3,10 @@ package com.swp391.OnlineEnglishLearningSystem.service;
 import com.swp391.OnlineEnglishLearningSystem.model.User;
 import com.swp391.OnlineEnglishLearningSystem.model.dto.UserDTO;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 //
 public interface UserService {
@@ -20,4 +23,14 @@ public interface UserService {
     boolean isOldPasswordCorrect(User currentUser, String oldPassword);
 
     User getById(Long userId);
+
+    List<User> getAllUsers();
+
+    User getUserById(Long id);
+
+    void deleteById(Long id);
+
+    Page<User> findPaginated(Pageable pageable);
+
+    Page<User> getUsersWithSpecs(Pageable pageale, String gender, String role, Boolean enabled, String search);
 }
