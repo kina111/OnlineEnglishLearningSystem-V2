f63dd23fd6b5c78ac90f387e8686efed7bf96676
Nguyen Kim Nam <namthptcvp@gmail.com>
2025-10-07 09:06:59 +0700

    #15 - Update RoleService

--- src/main/java/com/swp391/OnlineEnglishLearningSystem/service/RoleService.java
+++ src/main/java/com/swp391/OnlineEnglishLearningSystem/service/RoleService.java
@@ -1,4 +1,9 @@
 package com.swp391.OnlineEnglishLearningSystem.service;
 
+import com.swp391.OnlineEnglishLearningSystem.model.UserRole;
+
+import java.util.List;
+
 public interface RoleService {
+    List<UserRole> findAll();
 }
