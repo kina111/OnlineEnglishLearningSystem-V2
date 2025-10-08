bc64155c2c2e6e114afd7027bf6815b6bfacfc02
Nguyen Kim Nam <namthptcvp@gmail.com>
2025-10-08 09:28:33 +0700

    #17 - Logic for create, update, search and filter CourseCategory for admin management

--- src/main/java/com/swp391/OnlineEnglishLearningSystem/service/CourseCategoryService.java
+++ src/main/java/com/swp391/OnlineEnglishLearningSystem/service/CourseCategoryService.java
@@ -1,4 +1,20 @@
 package com.swp391.OnlineEnglishLearningSystem.service;
 
+import com.swp391.OnlineEnglishLearningSystem.model.CourseCategory;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface CourseCategoryService {
+    List<CourseCategory> findAll();
+
+    void ensureNotDuplicateName(String name);
+
+    void save(@Valid CourseCategory courseCategory);
+
+    CourseCategory getById(Long id);
+
+    Page<CourseCategory> getCourseCategoriesWithSpecs(Pageable pageable, Boolean active, String search);
 }
