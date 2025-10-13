5ab094a746affaa3dd3bbc717bdb1180c92dccb7
Nguyen Kim Nam <namthptcvp@gmail.com>
2025-10-13 10:48:31 +0700

    #17 - temp create question bank for quiz

--- src/main/java/com/swp391/OnlineEnglishLearningSystem/model/ShortAnswerOption.java
+++ src/main/java/com/swp391/OnlineEnglishLearningSystem/model/ShortAnswerOption.java
@@ -1,4 +1,22 @@
 package com.swp391.OnlineEnglishLearningSystem.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "short_answer_options")
 public class ShortAnswerOption {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Solution text cannot be empty")
+    @Size(max = 255, message = "Solution text is too long")
+    @Column(nullable = false, length = 255)
+    private String solutionText; // Nội dung đáp án đúng
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
 }
