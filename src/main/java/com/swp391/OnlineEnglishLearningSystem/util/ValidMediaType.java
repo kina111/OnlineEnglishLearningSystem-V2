e288fa408bccbfe3b19213a728048d640e2517ec
Nguyen Kim Nam <namthptcvp@gmail.com>
2025-10-16 12:47:09 +0700

    #19 - Created: new annotation for valid mediaType and media chosen by expert

--- src/main/java/com/swp391/OnlineEnglishLearningSystem/util/ValidMediaType.java
+++ src/main/java/com/swp391/OnlineEnglishLearningSystem/util/ValidMediaType.java
@@ -1,4 +1,18 @@
 package com.swp391.OnlineEnglishLearningSystem.util;
 
-public interface ValidMediaType {
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = ValidMediaTypeValidator.class) // Liên kết với logic validator
+@Target({ElementType.TYPE}) // Quan trọng: Chỉ định annotation này dùng cho CLASS
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidMediaType {
+
+    // Thông báo lỗi mặc định
+    String message() default "Loại file upload không khớp với MediaType đã chọn.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
