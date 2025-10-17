fda360d2fdac3d9a038a2df7e5c3ddc0b278c853
Nguyen Kim Nam <namthptcvp@gmail.com>
2025-10-17 15:34:57 +0700

    #19 - Created: Api for CRUD of chapter

--- src/main/java/com/swp391/OnlineEnglishLearningSystem/service/ChapterService.java
+++ src/main/java/com/swp391/OnlineEnglishLearningSystem/service/ChapterService.java
@@ -2,6 +2,7 @@ package com.swp391.OnlineEnglishLearningSystem.service;
 
 import com.swp391.OnlineEnglishLearningSystem.controller.ChapterController;
 import com.swp391.OnlineEnglishLearningSystem.model.Chapter;
+import jakarta.validation.Valid;
 
 import java.util.Optional;
 
@@ -9,4 +10,10 @@ public interface ChapterService {
     Chapter createChapterForCourse(Long courseId, ChapterController.CreateChapterRequest createChapterRequest);
 
     Optional<Chapter> findById(Long chapterId);
+
+    void deleteById(Long chapterId);
+
+    void deleteChapterAndReorder(Long courseId, Long chapterId);
+
+    Chapter updateChapter(Long chapterId, ChapterController.CreateChapterRequest chapter);
 }
