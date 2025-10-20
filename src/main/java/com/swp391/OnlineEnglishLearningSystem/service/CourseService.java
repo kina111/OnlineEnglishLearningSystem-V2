956902ab14d9323985f7facfe92b54bcb08cfc49
Nguyen Kim Nam <namthptcvp@gmail.com>
2025-10-20 14:22:11 +0700

    #25 - Updated: add methods to solve update, delete and sendSubmitReview

--- src/main/java/com/swp391/OnlineEnglishLearningSystem/service/CourseService.java
+++ src/main/java/com/swp391/OnlineEnglishLearningSystem/service/CourseService.java
@@ -3,16 +3,23 @@ package com.swp391.OnlineEnglishLearningSystem.service;
 
 import com.swp391.OnlineEnglishLearningSystem.model.Course;
 import com.swp391.OnlineEnglishLearningSystem.model.dto.CourseDTO;
+import com.swp391.OnlineEnglishLearningSystem.model.dto.UpdateCourseDTO;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface CourseService {
-    Course buildNewCourse(@Valid CourseDTO courseDTO);
+    Course buildNewCourse(@Valid CourseDTO courseDTO, Long authorId);
 
     void save(Course newCourse);
 
     Course findById(Long id);
 
     Page<Course> findCoursesByAuthorAndFilters(Long userId, Course.CourseStatus status, Long categoryId, String keyword, Pageable pageable);
+
+    Course updateCourse(Long id, @Valid UpdateCourseDTO updateDto);
+
+    void sendSubmitReview(Long courseId);
+
+    Course deleteById(Long courseId);
 }
--- src/main/java/com/swp391/OnlineEnglishLearningSystem/service/impl/CourseServiceImpl.java
+++ src/main/java/com/swp391/OnlineEnglishLearningSystem/service/impl/CourseServiceImpl.java
@@ -1,10 +1,14 @@
 package com.swp391.OnlineEnglishLearningSystem.service.impl;
 
 import com.swp391.OnlineEnglishLearningSystem.model.Course;
+import com.swp391.OnlineEnglishLearningSystem.model.User;
 import com.swp391.OnlineEnglishLearningSystem.model.dto.CourseDTO;
+import com.swp391.OnlineEnglishLearningSystem.model.dto.UpdateCourseDTO;
 import com.swp391.OnlineEnglishLearningSystem.repository.CourseCategoryRepository;
 import com.swp391.OnlineEnglishLearningSystem.repository.CourseRepository;
 import com.swp391.OnlineEnglishLearningSystem.service.CourseService;
+import com.swp391.OnlineEnglishLearningSystem.service.UploadService;
+import com.swp391.OnlineEnglishLearningSystem.service.UserService;
 import com.swp391.OnlineEnglishLearningSystem.service.specification.CourseSpecs;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,15 +20,25 @@ public class CourseServiceImpl implements CourseService {
 
     private CourseCategoryRepository courseCategoryRepository;
     private final CourseRepository courseRepository;
+    private final UploadService uploadService;
+    private final UserService userService;
 
-    public CourseServiceImpl(CourseCategoryRepository courseCategoryRepository, CourseRepository courseRepository) {
+    public CourseServiceImpl(CourseCategoryRepository courseCategoryRepository, CourseRepository courseRepository, UploadService uploadService, UserService userService) {
         this.courseCategoryRepository = courseCategoryRepository;
         this.courseRepository = courseRepository;
+        this.uploadService = uploadService;
+        this.userService = userService;
     }
 
     @Override
-    public Course buildNewCourse(CourseDTO courseDTO) {
+    public Course buildNewCourse(CourseDTO courseDTO, Long authorId) {
+        User author = this.userService.getUserById(authorId);
+        if (this.courseRepository.existsByName(courseDTO.getName())) throw new IllegalArgumentException("Course with name " + courseDTO.getName() + " already exists");
+        if (courseDTO.getThumbnailFile() == null && courseDTO.getThumbnailFile().isEmpty()) throw new IllegalArgumentException("Course thumbnail is required");
+
         Course course = new Course();
+        String thumbnailUrl = this.uploadService.uploadImage(courseDTO.getThumbnailFile(), "courses/thumbnails");
+        course.setThumbnail(thumbnailUrl);
         course.setName(courseDTO.getName());
         course.setCategory(courseCategoryRepository.getReferenceById(courseDTO.getCategoryId()));
         course.setShortDescription(courseDTO.getShortDescription());
@@ -32,10 +46,11 @@ public class CourseServiceImpl implements CourseService {
         course.setPrerequisite(courseDTO.getPrerequisite());
         course.setPrice(courseDTO.getPrice());
         course.setDiscount(courseDTO.getDiscount());
+        course.setAuthor(author);
 
         course.setFeatured(false);
         course.setStatus(Course.CourseStatus.DRAFT);
-        return course;
+        return this.courseRepository.save(course);
     }
 
     @Override
@@ -45,12 +60,46 @@ public class CourseServiceImpl implements CourseService {
 
     @Override
     public Course findById(Long id) {
-        return this.courseRepository.findById(id).orElseThrow();
+        return this.courseRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Course not found"));
     }
 
     @Override
     public Page<Course> findCoursesByAuthorAndFilters(Long userId, Course.CourseStatus status, Long categoryId, String keyword, Pageable pageable) {
         Specification<Course> spec = CourseSpecs.hasAuthorId(userId).and(CourseSpecs.hasStatus(status)).and(CourseSpecs.hasCategoryId(categoryId)).and(CourseSpecs.hasNameContaining(keyword));
         return this.courseRepository.findAll(spec, pageable);
+    }
+
+    @Override
+    public Course updateCourse(Long id, UpdateCourseDTO updateDto) {
+        Course course = this.courseRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Course not found"));
+        course.setName(updateDto.getName());
+        course.setShortDescription(updateDto.getShortDescription());
+        course.setDescription(updateDto.getDescription());
+        course.setPrerequisite(updateDto.getPrerequisite());
+        course.setPrice(updateDto.getPrice());
+        course.setDiscount(updateDto.getDiscount());
+        course.setCategory(courseCategoryRepository.getReferenceById(updateDto.getCategoryId()));
+        if (updateDto.getThumbnailFile() != null && !updateDto.getThumbnailFile().isEmpty()) {
+            String newThumbnailUrl = this.uploadService.uploadImage(updateDto.getThumbnailFile(), "courses/thumbnails");
+            course.setThumbnail(newThumbnailUrl);
+            updateDto.setCurrentThumbnailUrl(newThumbnailUrl);
+        }
+        return this.courseRepository.save(course);
+    }
+
+    @Override
+    public void sendSubmitReview(Long courseId) {
+        Course course = this.courseRepository.findById(courseId).orElseThrow(() -> new IllegalArgumentException("Course not found"));
+        if (course.getStatus() != Course.CourseStatus.DRAFT) throw new IllegalArgumentException("Course is not in draft status");
+        course.setStatus(Course.CourseStatus.PENDING);
+        this.courseRepository.save(course);
+    }
+
+    @Override
+    public Course deleteById(Long courseId) {
+        Course c = this.courseRepository.findById(courseId).orElseThrow(() -> new IllegalArgumentException("Course not found"));
+        if (c.getStatus() != Course.CourseStatus.DRAFT)  throw new IllegalArgumentException("Course is not in draft status");
+        this.courseRepository.delete(c);
+        return c;
     }
 }
