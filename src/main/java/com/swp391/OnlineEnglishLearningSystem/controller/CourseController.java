580b4dbaeac7d2a5b24c405e4a7021ccc4e5e8d6
Nguyen Kim Nam <namthptcvp@gmail.com>
2025-10-20 14:24:01 +0700

    #25 - Updated: update, delete, and sendSubmitReview apis

--- src/main/java/com/swp391/OnlineEnglishLearningSystem/controller/CourseController.java
+++ src/main/java/com/swp391/OnlineEnglishLearningSystem/controller/CourseController.java
@@ -3,10 +3,12 @@ package com.swp391.OnlineEnglishLearningSystem.controller;
 import com.swp391.OnlineEnglishLearningSystem.model.Course;
 import com.swp391.OnlineEnglishLearningSystem.model.CourseCategory;
 import com.swp391.OnlineEnglishLearningSystem.model.dto.CourseDTO;
+import com.swp391.OnlineEnglishLearningSystem.model.dto.UpdateCourseDTO;
 import com.swp391.OnlineEnglishLearningSystem.service.CourseCategoryService;
 import com.swp391.OnlineEnglishLearningSystem.service.CourseService;
 import com.swp391.OnlineEnglishLearningSystem.service.UploadService;
 import com.swp391.OnlineEnglishLearningSystem.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -65,42 +67,6 @@ public class CourseController {
         // 3. Return the name of the Thymeleaf template
         return "course/expertDashboard"; // Name of your HTML file (e.g., user-course-list.html)
     }
-    // ===================== CREATE COURSE ========================
-    @RequestMapping("/create")
-    public String getCreateCoursePage (Model model) {
-        List<CourseCategory> courseCategories = courseCategoryService.findAll();
-
-        model.addAttribute("courseCategories", courseCategories);
-        model.addAttribute("courseDTO", new CourseDTO());
-        return "course/createNewCourseForm";
-    }
-
-    @PostMapping("/create")
-    public String createCourse(@Valid @ModelAttribute("courseDTO") CourseDTO courseDTO,
-                             BindingResult bindingResult,
-                             RedirectAttributes redirectAttributes,
-                             Model model, Principal principal) {
-        if (bindingResult.hasErrors()) {
-            List<CourseCategory> courseCategories = courseCategoryService.findAll();
-            model.addAttribute("courseCategories", courseCategories);
-            return "course/createNewCourseForm";
-        }
-        try{
-            Course newCourse = courseService.buildNewCourse(courseDTO);
-            if (courseDTO.getThumbnailFile() != null && !courseDTO.getThumbnailFile().isEmpty()) {
-                String thumbnailFileName = uploadService.uploadImage(courseDTO.getThumbnailFile(), "courses/thumbnails");
-                newCourse.setThumbnail(thumbnailFileName);
-            }
-            newCourse.setAuthor(userService.findByEmailAndEnabledTrue(principal.getName()).orElseThrow());
-            courseService.save(newCourse);
-
-            model.addAttribute("course", newCourse);
-            return "course/viewAndUpdateChapter";
-        }catch (Exception e) {
-            redirectAttributes.addFlashAttribute("error", e.getMessage());
-            return "redirect:/courses/create";
-        }
-    }
 
     @GetMapping("/{id}")
     public String getCoursePage(Model model,
@@ -114,5 +80,103 @@ public class CourseController {
         }catch (Exception e) {
             return "home";
         }
+    }
+
+    // ===================== CREATE COURSE ========================
+    @RequestMapping("/create")
+    public String getCreateCoursePage (Model model) {
+        List<CourseCategory> courseCategories = courseCategoryService.findAll();
+        model.addAttribute("isUpdate", false);
+        model.addAttribute("isReadOnly", false);
+        model.addAttribute("courseCategories", courseCategories);
+        model.addAttribute("courseDTO", new CourseDTO());
+        return "course/createNewCourseForm";
+    }
+
+    @PostMapping("/create")
+    public String createCourse(@Valid @ModelAttribute("courseDTO") CourseDTO courseDTO,
+                               BindingResult bindingResult,
+                               RedirectAttributes redirectAttributes,
+                               Model model, HttpSession session) {
+        if (bindingResult.hasErrors()) {
+            List<CourseCategory> courseCategories = courseCategoryService.findAll();
+            model.addAttribute("courseCategories", courseCategories);
+            model.addAttribute("isUpdate", false);
+            model.addAttribute("isReadOnly", false);
+            return "course/createNewCourseForm";
+        }
+        try{
+            Long authorId = (Long) session.getAttribute("currentUserId");
+            Course newCourse = courseService.buildNewCourse(courseDTO, authorId);
+
+            model.addAttribute("course", newCourse);
+            return "course/viewAndUpdateChapter";
+        }catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
+            return "redirect:/courses/create";
+        }
+    }
+
+    //====================== UPDATE COURSE =========================
+    @GetMapping("/{id}/update")
+    public String showUpdateCourseForm(@PathVariable Long id, Model model) {
+        Course course = courseService.findById(id);
+
+        // Tạo UpdateDTO từ Course entity
+        UpdateCourseDTO updateDto = new UpdateCourseDTO(course);
+
+        model.addAttribute("courseDTO", updateDto); // Dùng tên chung để form tái sử dụng
+        model.addAttribute("isUpdate", true);
+        model.addAttribute("isReadOnly", updateDto.getStatus() != Course.CourseStatus.DRAFT);
+        model.addAttribute("courseId", id);
+        model.addAttribute("courseCategories", courseCategoryService.findAll());
+
+        return "course/createNewCourseForm";
+    }
+
+    @PostMapping("/{id}/update")
+    public String updateCourse(
+            @PathVariable Long id,
+            @Valid @ModelAttribute("courseDTO") UpdateCourseDTO updateDto,
+            BindingResult bindingResult,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("courseDTO", updateDto);
+            model.addAttribute("courseId", id);
+            model.addAttribute("isUpdate", true);
+            model.addAttribute("courseCategories", this.courseCategoryService.findAll());
+            return "course/createNewCourseForm";
+        }
+
+        Course coursetoUpdate = this.courseService.updateCourse(id, updateDto);
+        return "redirect:/courses/users/" + coursetoUpdate.getAuthor().getId();
+    }
+
+    //============= SUBMIT-REVIEW =================
+    @PostMapping("/{courseId}/submit-review")
+    public String sendSubmitReview(@PathVariable("courseId") Long courseId,
+                                   Model model,
+                                   HttpSession session, RedirectAttributes redirectAttributes){
+        try{
+            this.courseService.sendSubmitReview(courseId);
+            redirectAttributes.addFlashAttribute("message", "Send submit review success!");
+        }catch (Exception e){
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
+        }
+        return "redirect:/courses/users/" + session.getAttribute("currentUserId");
+    }
+
+    @PostMapping("/{courseId}/delete")
+    public String deleteCourse(@PathVariable("courseId") Long courseId,
+                               HttpSession session,
+                               RedirectAttributes redirectAttributes){
+        try{
+            Course courseToDelete = this.courseService.deleteById(courseId);
+            redirectAttributes.addFlashAttribute("message", "Delete course success!");
+        }catch (Exception e){
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
+        }
+        return "redirect:/courses/users/" + session.getAttribute("currentUserId");
     }
 }
