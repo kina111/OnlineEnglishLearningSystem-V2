7a3355195f7d803d7735954d562024ac2b697821
Nguyen Kim Nam <namthptcvp@gmail.com>
2025-10-21 14:17:24 +0700

    #32: Updated: method to get featured courses.

--- src/main/java/com/swp391/OnlineEnglishLearningSystem/repository/CourseRepository.java
+++ src/main/java/com/swp391/OnlineEnglishLearningSystem/repository/CourseRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
     Page<Course> findAll(Specification<Course> spec, Pageable pageable);
 
     boolean existsByName(String name);
+
+    List<Course> findAllByFeaturedTrue();
 }
