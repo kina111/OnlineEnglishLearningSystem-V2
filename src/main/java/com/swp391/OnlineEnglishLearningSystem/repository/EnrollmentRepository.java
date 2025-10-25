98de43e4d12356fdc4f7cc41e367b2c557eacdc6
Nguyen Kim Nam <namthptcvp@gmail.com>
2025-10-25 10:46:32 +0700

    #35: Updated: Enrollment repo and service to find by User and check enrolled.

--- src/main/java/com/swp391/OnlineEnglishLearningSystem/repository/EnrollmentRepository.java
+++ src/main/java/com/swp391/OnlineEnglishLearningSystem/repository/EnrollmentRepository.java
@@ -2,6 +2,14 @@ package com.swp391.OnlineEnglishLearningSystem.repository;
 
 import com.swp391.OnlineEnglishLearningSystem.model.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import com.swp391.OnlineEnglishLearningSystem.model.User;
+import java.util.List;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+    Enrollment findByCourseId(Long courseId);
+
+    @Query("SELECT e FROM Enrollment e JOIN FETCH e.course WHERE e.user = :user")
+    List<Enrollment> findByUserWithCourse(@Param("user") User user);
 }
--- src/main/java/com/swp391/OnlineEnglishLearningSystem/service/impl/EnrollmentServiceImpl.java
+++ src/main/java/com/swp391/OnlineEnglishLearningSystem/service/impl/EnrollmentServiceImpl.java
@@ -6,6 +6,7 @@ import com.swp391.OnlineEnglishLearningSystem.model.User;
 import com.swp391.OnlineEnglishLearningSystem.repository.EnrollmentRepository;
 import com.swp391.OnlineEnglishLearningSystem.service.EnrollmentService;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class EnrollmentServiceImpl implements EnrollmentService {
@@ -18,5 +19,18 @@ public class EnrollmentServiceImpl implements EnrollmentService {
     @Override
     public Enrollment createNew(Order order) {
         return this.enrollmentRepository.save(new Enrollment(order.getUser(), order.getCourse()));
+    }
+
+    @Override
+    public List<Enrollment> findByUser(User user) {
+        if (user == null) throw new IllegalArgumentException("User not found");
+        return this.enrollmentRepository.findByUserWithCourse(user);
+    }
+
+    public boolean isEnrolled(Long userId, Long courseId) {
+        if (userId == null || courseId == null) return false;
+        Enrollment temp = this.enrollmentRepository.findByCourseId(courseId);
+        if (temp == null) return false;
+        return temp.getUser().getId() == (userId);
     }
 }
