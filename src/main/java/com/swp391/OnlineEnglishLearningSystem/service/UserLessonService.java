f87d28647c9d133b4cc1629e2debc01195d2011e
Nguyen Kim Nam <namthptcvp@gmail.com>
2025-10-28 09:23:09 +0700

    #37: Created: UserLesson repo and service to update isCompleted and create full of UserLesson of a course to track learners.

---
+++ src/main/java/com/swp391/OnlineEnglishLearningSystem/service/UserLessonService.java
@@ -0,0 +1,14 @@
+package com.swp391.OnlineEnglishLearningSystem.service;
+
+import com.swp391.OnlineEnglishLearningSystem.model.Enrollment;
+import com.swp391.OnlineEnglishLearningSystem.model.Lesson;
+import com.swp391.OnlineEnglishLearningSystem.model.User;
+import com.swp391.OnlineEnglishLearningSystem.model.dto.UserLessonLearningDTO;
+
+import java.util.List;
+
+public interface UserLessonService {
+    void createFullUserLesson(Enrollment enrollment);
+
+    void updateIsCompleted(long userId, long lessonId);
+}
