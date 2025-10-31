7af26ba2127ddd38e063eff5c529d986cae20ec1
Nguyen Kim Nam <namthptcvp@gmail.com>
2025-10-31 11:07:42 +0700

    #40 - Created: Note repo, service

---
+++ src/main/java/com/swp391/OnlineEnglishLearningSystem/service/impl/NoteServiceImpl.java
@@ -0,0 +1,49 @@
+package com.swp391.OnlineEnglishLearningSystem.service.impl;
+
+import com.swp391.OnlineEnglishLearningSystem.model.Note;
+import com.swp391.OnlineEnglishLearningSystem.model.UserLesson;
+import com.swp391.OnlineEnglishLearningSystem.model.dto.NoteDTO;
+import com.swp391.OnlineEnglishLearningSystem.model.dto.NoteRequest;
+import com.swp391.OnlineEnglishLearningSystem.repository.NoteRepository;
+import com.swp391.OnlineEnglishLearningSystem.repository.UserLessonRepository;
+import com.swp391.OnlineEnglishLearningSystem.service.NoteService;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class NoteServiceImpl implements NoteService {
+    private final NoteRepository noteRepository;
+    private final UserLessonRepository userLessonRepository;
+
+    public NoteServiceImpl(NoteRepository noteRepository, UserLessonRepository userLessonRepository) {
+        this.noteRepository = noteRepository;
+        this.userLessonRepository = userLessonRepository;
+    }
+
+
+    @Override
+    public Note createNew(Long userId, long lessonId, NoteRequest noteRequest) {
+        UserLesson current = this.userLessonRepository.findByUserIdAndLessonId(userId, lessonId).orElseThrow(() -> new IllegalArgumentException("UserLesson not found"));
+        return this.noteRepository.save(new Note(current, noteRequest));
+    }
+
+    @Override
+    public List<NoteDTO> createDtosByChapterAndEnrollmentId(long enrollmentId, long chapterId, Sort sort) {
+        return this.noteRepository.findNotesByChapterAndEnrollmentId(chapterId, enrollmentId, sort);
+    }
+
+    @Override
+    public void deleteById(long noteId) {
+        this.noteRepository.deleteById(noteId);
+    }
+
+    @Override
+    public Note updateById(long noteId, String content) {
+        Note n = this.noteRepository.findById(noteId).orElseThrow(() -> new IllegalArgumentException("Note not found"));
+        n.setContent(content);
+        return this.noteRepository.save(n);
+    }
+}
